8f193fba07b598ab3838e309bb3ec53b9c7cfa42
Eric Schrijver <eric@ericschrijver.nl>
2014-05-16 17:25:41 +0200

    Settings also provide for means of dynamically requesting latest updated folder for Tiles, Forecasts

--- settings.py
+++ settings.py
@@ -14,28 +14,27 @@ TILE_SERVER        = "http://{s}.tile.openweathermap.org/map/precipitation_cls/{
 TILE_FOLDER        = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'raduga_tiles')
 GFS_FOLDER         = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static', 'gfs')
 
-LATEST_PREC_FOLDER = None
-LATEST_PREC_SLUG   = None
-LATEST_PREC_IMG    = None
-
-LATEST_GFS_FOLDER  = None
-LATEST_GFS_SLUG    = None
-
-# This is to find the latest folder of the form 2013-12-25T11:00:00
-for f in sorted(os.listdir(TILE_FOLDER), reverse=True):
-    # ['_earth.png', '_earth', '2013-12-25T11:00:00.png', '2013-12-25T11:00:00', '2013-12-24T12:00:00']
-    slug = f
-    path = os.path.join(TILE_FOLDER, f)
-    if re.match(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', slug) and os.path.isdir(path):
-        LATEST_PREC_FOLDER = path
-        LATEST_PREC_SLUG = slug
-        break
+def get_latest_prec_folder():
+    """
+    This is to find the latest folder of the form 2013-12-25T11:00:00
+    """
+    global TILE_FOLDER
+    for f in sorted(os.listdir(TILE_FOLDER), reverse=True):
+        # ['_earth.png', '_earth', '2013-12-25T11:00:00.png', '2013-12-25T11:00:00', '2013-12-24T12:00:00']
+        slug = f
+        path = os.path.join(TILE_FOLDER, f)
+        if re.match(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', slug) and os.path.isdir(path):
+            return path, slug
 
 # This is to find the latest folder of the form 2014022100
-for f in sorted(os.listdir(GFS_FOLDER), reverse=True):
-    slug = f
-    path = os.path.join(GFS_FOLDER, slug)
-    if re.match(r'\d{10}', slug) and os.path.isdir(path) and len(glob(os.path.join(path, '*pwat.grib'))) > 0:
-        LATEST_GFS_FOLDER = path
-        LATEST_GFS_SLUG   = slug
-        break
+def get_latest_gfs_folder():
+    global GFS_FOLDER
+    for f in sorted(os.listdir(GFS_FOLDER), reverse=True):
+        slug = f
+        path = os.path.join(GFS_FOLDER, slug)
+        if re.match(r'\d{10}', slug) and os.path.isdir(path) and len(glob(os.path.join(path, '*pwat.grib'))) > 0:
+            return path, slug
+
+LATEST_PREC_FOLDER, LATEST_PREC_SLUG = get_latest_prec_folder()
+LATEST_GFS_FOLDER, LATEST_GFS_SLUG   = get_latest_gfs_folder()
+
