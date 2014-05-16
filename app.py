762b548a0651272d988c3e36c5692b271902844f
Eric Schrijver <eric@ericschrijver.nl>
2014-05-16 18:14:22 +0200

    URL that redirects to latest Elektro-L image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 import os
 
 # Dependencies: Flask + PIL or Pillow
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect
 
 # Local imports
 from settings import *
@@ -16,6 +16,9 @@ app = Flask(__name__)
 def base_static(filename):
     return send_from_directory(os.path.join(app.root_path, 'raduga_tiles'), filename)
 
+@app.route("/latest/elektro-l")
+def latest_elektro():
+    return redirect(get_latest_elektro_l_url())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ TILE_SIZE = 256
 TILE_SERVER        = "http://{s}.tile.openweathermap.org/map/precipitation_cls/{z}/{x}/{y}.png"
 TILE_FOLDER        = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'raduga_tiles')
 GFS_FOLDER         = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static', 'gfs')
+ELEKTRO_L_FOLDER         = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static', 'elektro')
 
 def get_latest_prec_folder():
     """
@@ -34,6 +35,11 @@ def get_latest_gfs_folder():
         path = os.path.join(GFS_FOLDER, slug)
         if re.match(r'\d{10}', slug) and os.path.isdir(path) and len(glob(os.path.join(path, '*pwat.grib'))) > 0:
             return path, slug
+
+def get_latest_elektro_l_url():
+    global ELEKTRO_L_FOLDER
+    for f in sorted(os.listdir(ELEKTRO_L_FOLDER), reverse=True):
+        return "/static/elektro/" + f
 
 LATEST_PREC_FOLDER, LATEST_PREC_SLUG = get_latest_prec_folder()
 LATEST_GFS_FOLDER, LATEST_GFS_SLUG   = get_latest_gfs_folder()
